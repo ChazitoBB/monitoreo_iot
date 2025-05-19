5aae2d13dd8183e7c3332d8ad57f2b9510accc29
ChazitoBB <chaz313@hotmail.com>
2025-05-19 11:27:34 -0600

    Actualizacion de ip

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const tableBody = document.getElementById('deviceTableBody');
 const firstStatus = document.getElementById('firstStatus');
-const API_URL = "http://98.83.159.59/api/devices"; // URL de la API
+const API_URL = "http://18.212.145.153/api/devices"; // URL de la API
 
 async function fetchData() {
     try {
